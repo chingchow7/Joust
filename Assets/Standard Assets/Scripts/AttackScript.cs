a265e82c99027bf58025fe84a075b2d4649dff65
Ching Chow <chingchow7@gmail.com>
2015-04-29 02:07:43 -0500

    basic anim

---
+++ Assets/Standard Assets/Scripts/AttackScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using SmoothMoves;
+
+public class AttackScript : MonoBehaviour {
+
+	private BoneAnimation 	 boneAnimation;
+	private Animation		 animation;
+
+	// Use this for initialization
+	void Awake () {
+		boneAnimation = GetComponent<BoneAnimation>();
+		animation = boneAnimation.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			boneAnimation.Play ("Attack");
+		}
+		//Debug.Log ("frame # is " + boneAnimation["Attack"].frame);
+	}
+}
---
+++ Assets/Standard Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float y_speed = .5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		transform.Translate( new Vector3( 0, y_speed, 0));
+	}
+}
